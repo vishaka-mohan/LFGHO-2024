0f3eae9aea7e5892b4171c8b29e55905085f8033
Vishaka Mohan <vishaka.mohan2019@vitstudent.ac.in>
2024-01-16 14:03:25 +0530

    borrowGHO setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { Pool, InterestRate, EthereumTransactionTypeExtended } from "@aave/contract-helpers";
 import { BigNumber, ethers } from 'ethers';
 import tokenABI from './contract/GHOAbi.json'
+import AAVE_ABI from "./contract/aave_abi"
 
 function App() {
 
@@ -14,24 +15,7 @@ function App() {
 
   
 
-  // const pool = new Pool(provider, {
-  //   POOL: "0x3De59b6901e7Ad0A19621D49C5b52cC9a4977e52", // sepolia GHO market
-  //   WETH_GATEWAY: "0x9c402E3b0D123323F0FCed781b8184Ec7E02Dd31", // sepolia GHO market
-  // });
   
-
-  // async function submitTransaction(tx: EthereumTransactionTypeExtended){
-  //   const extendedTxData = await tx.tx();
-  //   // const { from, ...txData } = extendedTxData;
-  //   // const signer = provider.getSigner(from);
-  //   // const txResponse = await signer.sendTransaction({
-  //   //   ...txData,
-  //   //   value: txData.value ? BigNumber.from(txData.value) : undefined,
-  //   // });
-  //   // console.log(txResponse)
-  // }
-
-
 
   const sendGHO = async () => {
     const howMuchTokens = ethers.utils.parseUnits('1', 18)
@@ -40,46 +24,44 @@ function App() {
 
   }
 
+  const borrowGHO = async () => {
+    const aave = new ethers.Contract("0x6Ae43d3271ff6888e7Fc43Fd7321a503ff738951", AAVE_ABI, signer);
+
+    const tx = await aave.borrow(
+      "0xc4bF5CbDaBE595361438F8c6a187bDc330539c60",
+      ethers.utils.parseUnits("2", 18),
+      2,
+      0,
+      await signer.getAddress(),
+    );
+    const transaction = await tx.wait();
+    console.log(transaction)
+  }
+
+
+  const supplyGHOAsCollateral = async () => {
+    //to do
+  }
+
   useEffect(() => {
 
-    const executeMint = async () => {
-      //write this line on first page load
-      //await provider.send("eth_requestAccounts", []);
-    
-      
-      // const signer = provider.getSigner(accounts[0])
-      // const contractSigner = tokenContract.connect(signer)
-      
-
-      
-
-      // const txs: EthereumTransactionTypeExtended[] = await pool.borrow({
-      //   user: "0xB0138E967807ccdA91a7aA9abd1d2183cC3D2260",
-      //   reserve: "0xcbE9771eD31e761b744D3cB9eF78A1f32DD99211", // Goerli GHO market
-      //   amount: "1",
-      //   interestRateMode: InterestRate.Stable,
-      //   debtTokenAddress: "0x80aa933EfF12213022Fd3d17c2c59C066cBb91c7"
-      // })
-
-     
-      // console.log(txs)
-      // submitTransaction(txs[0])
-
-
-
-
-    }
-
-    executeMint()
-
-
+    //write this line on first page load
+    //await provider.send("eth_requestAccounts", []);
     
   }, [])
 
 
   return (
     <div className="App">
-      hello
+      hello. Next steps.
+      <ol>
+        <li>Implement sendGHO, borrowGHO, supplyGHO, repayGHO, credit delegation as methods to be used by others right out of the box</li>
+        <li>Implement snap - set up GPT API</li>
+        <li>Implement subscription contractwith frontend + make SDK method for easy usage</li>
+        <li>Integrate subscription with snaps</li>
+        <li>Look for other operations like swaps andcross chain stuff with chainlink ccip</li>
+        <li>Try to use Family</li>
+      </ol>
     </div>
   );
 }
